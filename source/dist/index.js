9825c2749410157c53ad6db33f10702b83671137
Woshiajuana <zhigang.chen@owulia.com>
2018-03-26 16:48:32 +0800

    cell

--- source/dist/index.js
+++ source/dist/index.js
@@ -1,11 +1,13 @@
-import WowView from './../lib/wow-view'
-import WowButton from './../lib/wow-button'
-import WowCarousel from './../lib/wow-carousel'
-import WowInput from './../lib/wow-input'
+import WowView      from './../lib/wow-view'
+import WowButton    from './../lib/wow-button'
+import WowCarousel  from './../lib/wow-carousel'
+import WowInput     from './../lib/wow-input'
+import WoWCell      from './../lib/wow-cell'
 
 export {
     WowView,
     WowButton,
     WowCarousel,
     WowInput,
+    WoWCell,
 }
