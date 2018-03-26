4311d6d9e51d8a12ee644531dc090335ca39542f
Woshiajuana <zhigang.chen@owulia.com>
2018-03-26 16:12:59 +0800

    提交v0.0.8

--- source/dist/index.js
+++ source/dist/index.js
@@ -1,7 +1,11 @@
 import WowView from './../lib/wow-view'
 import WowButton from './../lib/wow-button'
+import WowCarousel from './../lib/wow-carousel'
+import WowInput from './../lib/wow-input'
 
 export {
     WowView,
     WowButton,
+    WowCarousel,
+    WowInput,
 }
