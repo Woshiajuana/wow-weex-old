9b03ebeea50f288e25f48305dafd2a3119676552
czg <czg@jfpal.com>
2018-04-08 10:50:33 +0800

    更新0.0.19 wow-checkbox组件

---
+++ source/lib/wow-checkbox/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="checkbox-wrap" @click="handleClick">
+        <image class="checkbox-icon" v-if="checkbox_active_src" :src="checkbox_switch ? checkbox_active_src : checkbox_default_src"></image>
+        <slot></slot>
+    </div>
+</template>
+<script>
+    import source from 'utils/source'
+    export default{
+        props: {
+            checkbox_switch: { default: false },
+            checkbox_active_src: { default: source('ds-checkbox-active-icon.png') },
+            checkbox_default_src: { default: source('ds-checkbox-default-icon.png') }
+        },
+        methods: {
+            handleClick() {
+                this.$emit('click')
+            }
+        }
+    }
+</script>
+<style>
+    .checkbox-wrap{
+        flex-direction: row;
+        align-items: center;
+    }
+    .checkbox-icon{
+        width: 40px;
+        height: 40px;
+    }
+</style>
