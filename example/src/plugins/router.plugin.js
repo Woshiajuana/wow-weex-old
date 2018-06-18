5289f9dc2b93f8b6e3e9436fa3506e75c0f86f29
czg <czg@jfpal.com>
2018-06-18 16:32:31 +0800

    更新插件模块

--- example/src/plugins/router.plugin.js
+++ example/src/plugins/router.plugin.js
@@ -1,54 +1,32 @@
+/**
+ * navigator
+ * weex 自带模块
+ *
+ */
 
-import './Promise'
-import path             from 'plugins/path.plugin'
-import loading          from 'plugins/loading.plugin'
-import dialogs          from 'plugins/dialogs.plugin'
-import Defer            from 'utils/defer.util'
-import _                from 'utils/lodash.util'
+import ResourcePlugin               from 'plugins/resource.plugin'
 
-const navigator = weex.requireModule('navigator');
-const manager = weex.requireModule('pageManagerModule');
-const external = weex.requireModule('externalAppModule');
-const defer = new Defer(1000);
+const Navigator         = weex.requireModule('navigator');
 
 export default {
-    push: (input, parmas) => {
-        if (!defer.do(input + JSON.stringify(parmas))) return null; // 过滤连续点击
-        loading.show();
-        let pagename = _.isObject(input) ? input.url : input;
-        let animated = _.isObject(input) ? input.animated:  'true';
-        let close = _.isObject(input) ? input.close:  '';
-        path.page(pagename).then(nativepath => {
-            nativepath = parmas ? nativepath + '?parmas=' + encodeURIComponent(JSON.stringify(parmas)) : nativepath;
-            navigator.push({ url: nativepath, animated: animated, close: close }, e => {});
-        }).catch(error => {
-            dialogs.toast({ message: error });
-        }).finally(() => { loading.hide(); });
-    },
 
-    pop: (animated = 'true') => {
-        navigator.pop({ animated: animated }, e => {});
-    },
+    /**
+     * 跳转页面
+     * @param options   [object|string] 参数配置，页面key
+     * @param params    [object]        页面传参
+     * */
+    push: (options, params) => new Promise(async (resolve, reject) => {
+        try {
+            let key = options.key || options;
+            let animated = options.animated || 'true';
+            let close = options.close || 'false';
+            let url = await ResourcePlugin.get(key);
+            !params && (url = `${url}?params=${encodeURIComponent(JSON.stringify(params))}`);
+            Navigator.push({url, animated, close});
+        } catch (e) {
+            reject(e);
+        } finally {
 
-    root: () => {
-        manager.popToRootPage(e => {})
-    },
-
-    getParams: weex => {
-        var bundleUrl = weex.$getConfig().bundleUrl,
-            params = bundleUrl.indexOf('?parmas=') > -1 ? bundleUrl.substr(bundleUrl.indexOf("?parmas=") + 8) : '';
-        return params ? JSON.parse(decodeURIComponent(decodeURIComponent(params))) : '';
-    },
-
-    back: (back_index = 1, front_index = '') => {
-        manager.popWithParam({popBack: back_index + '', popFront: front_index + ''}, e => {})
-    },
-
-    del: (back_index = 1, front_index = '') => {
-        manager.deletePageWithParam({popBack: back_index + '', popFront: front_index + ''}, e => {})
-    },
-
-    external: options => {
-        external.open(options);
-    }
-};
+        }
+    }),
+}
